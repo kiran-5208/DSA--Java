eb890372389ec5320797f3adde4e56bf821deff9
Kiran Goudra <kirangoudra7204@gmail.com>
2026-06-01 19:31:43 +0530

    Implement Longest Palindromic Substring finder

---
+++ Strings/01_LongestPalindromicSubstring.java
@@ -0,0 +1,40 @@
+package Strings;
+
+public class LongestPalindromicSubstring
+  {
+    static boolean isPalindrome(String str)
+    {
+      int i = 0;
+      int j = str.length() - 1;
+      while (i < j)
+        {
+          if (str.charAt(i) != str.charAt(j))
+          {
+            return false;
+          }
+            i++;
+            j--;
+        }
+          return true;
+    }
+   public static void main(String[] args) 
+    {
+      String str = "babbad";
+      int maxLength = 0;
+      String longestPalindrome = "";
+      for (int i = 0; i < str.length(); i++) 
+      {
+        for (int j = i + 1; j <= str.length(); j++)
+          {
+            String temp = str.substring(i, j);
+            if (isPalindrome(temp) && temp.length() > maxLength) {
+                    maxLength = temp.length();
+                    longestPalindrome = temp;
+                }
+            }
+        }
+
+        System.out.println("Longest Palindrome: " + longestPalindrome);
+        System.out.println("Length: " + maxLength);
+    }
+}
